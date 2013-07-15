e4849f47dd55c68ef1809d4228f07e4ba4752795
Emilio Cecconi <emiliocecconi@gmail.com>
2013-07-15 12:59:15 -0500

    Added -y to node.js install

--- setup.sh
+++ setup.sh
@@ -47,6 +47,6 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 # Install Node.js and npm
 sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
